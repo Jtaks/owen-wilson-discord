538db3c7199c7d9c8bb23b1b3efc7fdcdf8cb027
Jesse <jesse.liptak@gmail.com>
2022-08-26 05:35:51 +0000

    Update cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,22 +1,30 @@
 import { program } from "commander";
-
-program.option(
-  "-p, --production",
-  "Production indicates non-Guild application commands"
-);
+import { registerCommands, removeOldCommands } from "../lib/command-manager";
 
 program
-  .command("register [command...]")
-  .description("Register given commands. Register all if omitted.")
-  .action((commands) => {
-    console.log(`todo: remove commands ${commands}`);
+  .option(
+    "-p, --production",
+    "Production indicates non-Guild application commands. This overrides the NODE_ENV environment variable."
+  )
+  .hook("preAction", () => {
+    if (program.opts().production) {
+      process.env.NODE_ENV = "production";
+    }
   });
 
 program
-  .command("remove [command...]")
-  .description("Remove given commands. Remove all if omitted")
-  .action((commands) => {
-    console.log(`todo: remove commands ${commands}`);
+  .command("register")
+  .description("Register all commands.")
+  .action(() => {
+    registerCommands();
+  });
+
+program
+  .command("remove")
+  .description("Remove old commands")
+  .action(() => {
+    // TODO: Remove all guild commands
+    removeOldCommands();
   });
 
 program.parse();
